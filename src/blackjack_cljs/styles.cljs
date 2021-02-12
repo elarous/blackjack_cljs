d1e868a6abe66b0699d07c53248dfee27a2224a6
Oussama El Arbaoui <elarbaouioussama@gmail.com>
2021-02-12 13:52:36 +0100

    add scores card UI

--- src/blackjack_cljs/styles.cljs
+++ src/blackjack_cljs/styles.cljs
@@ -1,16 +1,51 @@
 (ns blackjack-cljs.styles)
 
+(def colors
+  {:green          "#8FE388"
+   :red            "#E26D5A"
+   :primary        "#43BCCD"
+   :primary-text   "#2F131E"
+   :secondary-text "#240115"
+   :grey-lighter   "#F5F5F5"
+   :grey-light     "#EEEEEE"})
+
+(defn sizes [num]
+  (str (* num 8) "px"))
+
 (defn container []
-  {:display "flex"})
+  {:display               "grid"
+   :grid-template-columns "20% 60% 20%"})
 
 (defn deck []
   {:position "relative"})
 
-
 (defn deck-card [left shadow]
-  {:left       (str left "px")
-   :box-shadow (if shadow "4px 4px 8px rgba(0,0,0,1)" "none")
+  {:left          (str left "px")
+   :box-shadow    (if shadow "4px 4px 8px rgba(0,0,0,1)" "none")
    :border-radius "15px"
-   :border "1px solid black"
-   :position   "absolute"})
+   :border        "1px solid black"
+   :position      "absolute"})
 
+(defn stats-card []
+  {:background-color (:grey-lighter colors)
+   :padding          (str (sizes 1) " " (sizes 4))
+   :border-radius    "5px"
+   :font-family      "Indie Flower"})
+
+(defn stats-card-item []
+  {:display         "flex"
+   :justify-content "space-between"
+   :align-items     "center"
+   :font-size       (sizes 3)
+   :margin          (sizes 1)})
+
+(defn stats-card-lbl []
+  {:color (:primary colors)})
+
+(defn stats-card-wins []
+  {:color     (:green colors)
+   :font-size (sizes 4)})
+
+(defn stats-card-losses []
+  {:color     (:red colors)
+   :font-size (sizes 4)})
--- src/blackjack_cljs/views.cljs
+++ src/blackjack_cljs/views.cljs
@@ -3,8 +3,6 @@
             [herb.core :refer [<class]]
             [blackjack-cljs.styles :as stl]))
 
-
-
 (defn deck-column []
   [:div
    [:div {:class (<class stl/deck)}
@@ -13,10 +11,19 @@
     [:img {:src   "imgs/cards/1B.svg"
            :class (<class stl/deck-card 10 false)}]]])
 
+(defn game-column []
+  [:div])
 
-(defn game-column [])
+(defn stats-column []
+  [:div
+   [:div {:class (<class stl/stats-card)}
+    [:div {:class (<class stl/stats-card-item)}
+     [:div {:class (<class stl/stats-card-lbl)} "Wins"]
+     [:div {:class (<class stl/stats-card-wins)} "1000"]]
+    [:div {:class (<class stl/stats-card-item)}
+     [:div {:class (<class stl/stats-card-lbl)} "Losses"]
+     [:div {:class (<class stl/stats-card-losses)} "0"]]]])
 
-(defn stats-column [])
 
 (defn container [& columns]
   [:div {:class (<class stl/container)} columns])
